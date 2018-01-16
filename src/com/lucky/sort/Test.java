84a48adc3a6b5b7514d787dd8d1aebefaa575cb8
allen <q1616163@163.com>
2018-01-16 18:38:12 +0800

    1.16

---
+++ src/com/lucky/sort/Test.java
@@ -0,0 +1,22 @@
+package com.lucky.sort;
+
+public class Test {
+	public static void swap1(int x){
+		System.out.println("x="+x);
+	}
+	public static void swap2(int x){
+		System.out.println("x="+x);
+	}
+	public static void main(String[] args) {
+		int x=0;
+		for (int i = 0; i < 3; i++) {
+			swap1(++x);
+		}
+		System.out.println(x);
+		x=0;
+		for (int i = 0; i < 3; i++) {
+			swap1(x+++1);
+		}
+		System.out.println(x);
+	}
+}
