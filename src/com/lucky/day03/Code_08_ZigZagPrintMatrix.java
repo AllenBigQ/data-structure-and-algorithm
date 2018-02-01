aebccac1ef5255c5f4270a5a8e16ac7e7d659203
allen <q1616163@163.com>
2018-02-01 23:41:55 +0800

    2.1

--- src/com/lucky/day03/Code_08_ZigZagPrintMatrix.java
+++ src/com/lucky/day03/Code_08_ZigZagPrintMatrix.java
@@ -3,20 +3,24 @@ package com.lucky.day03;
 public class Code_08_ZigZagPrintMatrix {
 
 	public static void printMatrixZigZag(int[][] matrix) {
-		int tR = 0;
-		int tC = 0;
-		int dR = 0;
-		int dC = 0;
-		int endR = matrix.length - 1;
-		int endC = matrix[0].length - 1;
+		int row1 = 0;
+		int col1 = 0;
+		int row2 = 0;
+		int col2 = 0;
+		int endRow = matrix.length - 1;
+		int endCol = matrix[0].length - 1;
 		//fromUp刚开始为false，表示从左下方向右上方的顺序开始打印
 		boolean fromUp = false;
-		while (tR != endR + 1) {
-			printLevel(matrix, tR, tC, dR, dC, fromUp);
-			tR = tC == endC ? tR + 1 : tR;
-			tC = tC == endC ? tC : tC + 1;
-			dC = dR == endR ? dC + 1 : dC;
-			dR = dR == endR ? dR : dR + 1;
+		while (row1 != endRow + 1) {
+			printLevel(matrix, row1, col1, row2, col2, fromUp);
+			//当col1等于endCol时，说明已经移动到最右边，row1+1，开始向下移动
+			row1 = col1 == endCol ? row1 + 1 : row1;
+			//当col1不等于endCol时，说明还没移动到最右，col1+1向右移动
+			col1 = col1 == endCol ? col1 : col1 + 1;
+			//当row2等于endRow时，说明已经移动到最下，col2+1向右移动
+			col2 = row2 == endRow ? col2 + 1 : col2;
+			//当row2等于endRow时，说明已经移动到最下，row2不变
+			row2 = row2 == endRow ? row2 : row2 + 1;
 			fromUp = !fromUp;
 		}
 		System.out.println();
